4b1c216ffecc63a749fecfab0e09883c03873b00
Patrick Han <patric1khan@gmail.com>
2020-08-13 20:34:08 -0500

    vec3 class definition

--- rt/Source.cpp
+++ rt/Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "vec3.h"
 
 
 int main() {
@@ -14,6 +15,9 @@ int main() {
 
 	// Pixels are written left to right, rows from top to bottom
 	for (int j = image_height - 1; j >= 0; --j) {
+		// Progress bar
+		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+
 		for (int i = 0; i < image_width; ++i) {
 			auto r = double(i) / (image_width - 1);
 			auto g = double(j) / (image_height - 1);
@@ -27,4 +31,6 @@ int main() {
 			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
 		}
 	}
+
+	std::cerr << "\nDone.\n";
 }
